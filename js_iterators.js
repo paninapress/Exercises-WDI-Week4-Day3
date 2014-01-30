5b7e8ff9807addfe68543f58782c18315793c263
paninapress <NPacifico25@gmail.com>
2014-01-29 19:14:13 -0800

    nailed it

---
+++ js_iterators.js
@@ -0,0 +1,116 @@
+// create a namespace for our iterator functions
+var Iterators = (function() {
+  return {
+    each: function (arr, action) {
+      for (var i=0; i < arr.length; i++)
+      {
+        action(arr[i]);
+      }
+      return arr;
+      // implment the each iterator
+      // such that action is applied
+      // to each element on the array
+      // to which this function was applied
+      // this function should also return the 
+      // original array untouched
+    },
+    map: function(arr, action) {
+      // var NewArr = [];
+      // for (var i=0; i < arr.length; i++)
+      // {
+      //   NewArr.push(action(arr[i]));
+      // }
+      // return NewArr;
+
+      var NewArr = [];
+      Iterators.each(arr, function(x){
+        NewArr.push(action(x));
+      });
+      return NewArr;
+
+
+      // implement map iterator
+      // such that action is applied
+      // to each element on the array
+      // returning a new array containing
+      // the results of the applications
+    },
+    reduce: function(arr, base, action) {
+      // for (var i=0; i < arr.length; i++)
+      // {
+      //   var x = base;
+      //   base = action(arr[i], x);
+      // }
+      // return base;
+      var i = base;
+      Iterators.each(arr, function(x) {
+        
+        
+        i = action(i, x);
+
+
+
+      });
+      return i;
+      // as a BONUS, implement reduce
+      // btw, reduce is an alias for inject :)
+      
+      // this function should take an array, a 
+      // base value, and a combination action
+      // function -- for example add and perform
+      // the action on each successive element
+    }
+  };
+})();
+
+// declare the array
+var arr = [1,2,3];
+
+// should output
+// 1
+// 2
+// 3
+console.log("results of applying Iterators.each to arr:");
+Iterators.each(arr, function (number) {
+  console.log(number);
+});
+
+// should output
+// => [1,2,3]
+console.log("applying Iterators.each to arr should return the array:");
+var result = Iterators.each(arr, function (number) {
+});
+
+console.log(result);
+// should output
+// [2, 4, 6]
+console.log("results of applying Iterators.map to arr:");
+console.log(
+  Iterators.map(arr, function (number) {
+    return number * 2;
+  })
+);
+
+// should output
+// false to indicate that the array returned
+// by map is a different object than the array
+// that had been passed in
+console.log("arr and the array returned by map are identical:");
+console.log(
+  arr === Iterators.map(arr, function (number) {
+    return number * 2;
+  })
+);
+
+// should output arr's contents to indicate that map did not 
+// modify the original array's contents
+console.log("arr's contents:");
+console.log(arr);
+
+// BONUS: should output the sum
+console.log("result of applying Iterators.reduce to arr with a sum function:");
+console.log(
+  Iterators.reduce(arr, 0, function (a, b) {
+    return a + b;
+  })
+);
